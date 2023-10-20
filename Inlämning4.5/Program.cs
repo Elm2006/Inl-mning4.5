9d2772f58990ace146efdebbb65f710d6d44a945
isel06002 <isel06002@E-PW0234HX>
2023-10-20 09:01:41 +0200

    Add project files.

---
+++ Inlämning4.5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Inlämning_4.__5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string tal = Console.ReadLine();
+            string talet = "";
+            for (int i = 0; i < tal.Length; i++)
+            {
+                int nior = tal.IndexOf("9");
+                Console.WriteLine(tal[i]);
+                string tecken = tal[i].ToString();
+                int siffra = int.Parse(tecken);
+                if (i == nior)
+                {
+                    siffra = 0;
+                }
+                if(i+1 == tal.Length)
+                {
+                    siffra += 1;
+                }
+                talet += "" + siffra;
+            }
+            Console.WriteLine(talet);
+        }
+    }
+}
